5c58643a81ebbcb3c0de518ac19a4188c4d1eb25
Victor Augusto de Souza da Silva <victoraugustosouzasilva4232@gmail.com>
2025-09-01 21:19:27 -0300

    criacao do projeto e organizacao das pastas

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,5 @@
+import { style } from "./styles";
+
+export function Home(){
+    
+}
---
+++ src/screens/Home/styles.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+
+    contatiner:{
+        flex: 1,
+        backgroundColor: "#7A4A9E"
+        
+    }
+})
